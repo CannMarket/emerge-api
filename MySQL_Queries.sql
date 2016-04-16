ee91e2a42b3bad61f3db7c7ea4f11019cb8e1780
Haxathalon <webmast84@gmail.com>
2016-04-16 13:01:51 -0400

    Initial SQL Create Statements
    These are initial tables for the app, to be improved.

---
+++ MySQL_Queries.sql
@@ -0,0 +1,55 @@
+
+SELECT SHA2('beep', 256);
+
+CREATE DATABASE moshe;
+
+USE moshe;
+
+########################### USERS TABLE ##############################
+CREATE TABLE users (
+	id INT(11) AUTO_INCREMENT NOT NULL,
+    fname VARCHAR(100) NOT NULL,
+	lname VARCHAR(100) NOT NULL,
+	email VARCHAR(150) NOT NULL,
+    pass VARCHAR(70) NOT NULL,
+	phone VARCHAR(20) NOT NULL,
+	address1 VARCHAR(255) NOT NULL,
+	address2 VARCHAR(100) NOT NULL,
+	city VARCHAR(100) NOT NULL,
+	state VARCHAR(100) NOT NULL,
+	zipcode VARCHAR(20) NOT NULL,
+	country VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id),
+    UNIQUE KEY(email),
+    UNIQUE KEY(phone)
+);
+
+########################### CONTACTS TABLE ##############################
+ CREATE TABLE contacts (
+	id INT(11) AUTO_INCREMENT NOT NULL,
+    uid INT(11) NOT NULL,
+    cid INT(11) NOT NULL,
+	PRIMARY KEY(id),
+    UNIQUE KEY `Connected_Contact` (uid,cid)
+ );
+ 
+ ########################### PAYMENT CARDS TABLE ##############################
+ 
+ CREATE TABLE cards (
+		id INT(11) AUTO_INCREMENT NOT NULL,
+		uid INT(11) NOT NULL,
+		CardName VARCHAR(255) NOT NULL,
+		CardNumber VARCHAR(50) NOT NULL,
+		acquiringBin VARCHAR(30) NOT NULL,
+		acquirerCountryCode INT(5) NOT NULL,
+        expirationData VARCHAR(15) NOT NULL,
+        currencyCode INT(5) NOT NULL,
+        PRIMARY KEY(id),
+		UNIQUE KEY (CardNumber)
+ );
+ 
+ ########################### PAYMENT TOKENS TABLE ##############################
+ # THESE ARE UNIQUE TOKENS FOR SENDING PAYMENT REQUESTS AND TRACKING WHICH "TOKENS" ARE EFFECTIVE FOR Donations for example.alter
+ # THIS IS AFTER BASIC IMPLEMENTATION
+    
+    
