cc80fa5c451c7cd8783b4e86540dcc813b4831eb
alienhax <eMerge2015@alienhax.com>
2016-04-17 01:57:46 -0400

    changed endpoints for authenticate/pullfunds

--- jot-bot/app/Http/routes.php
+++ jot-bot/app/Http/routes.php
@@ -15,6 +15,14 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::group(['prefix' => '/'], function () {
+    Route::resource('authenticate', 'AuthenticateController', ['only' => ['index']]);
+	Route::post('authenticate', 'AuthenticateController@authenticate');
+
+	Route::resource('pullfunds', 'VisaAuthenticateController', ['only' => ['index']]);
+	Route::post('pullfunds', 'VisaAuthenticateController@index');
+});
+
 Route::group(['prefix' => 'api'], function()
 {
 	Route::resource('authenticate', 'AuthenticateController', ['only' => ['index']]);
