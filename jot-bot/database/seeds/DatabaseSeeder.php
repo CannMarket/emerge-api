6a8ec6526f7b5a1f129e3d57d349e2e0e7c535e0
Haxathalon <webmast84@gmail.com>
2016-04-17 01:44:49 -0400

    Updated Users Seed
    Added more fields to user seed

--- jot-bot/database/seeds/DatabaseSeeder.php
+++ jot-bot/database/seeds/DatabaseSeeder.php
@@ -18,10 +18,10 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->delete();
 
         $users = array(
-                ['name' => 'Ryan Chenkie', 'email' => 'ryanchenkie@gmail.com', 'password' => Hash::make('secret')],
-                ['name' => 'Chris Sevilleja', 'email' => 'chris@scotch.io', 'password' => Hash::make('secret')],
-                ['name' => 'Holly Lloyd', 'email' => 'holly@scotch.io', 'password' => Hash::make('secret')],
-                ['name' => 'Adnan Kukic', 'email' => 'adnan@scotch.io', 'password' => Hash::make('secret')],
+                ['name' => 'Jeremy Smith', 'email' => 'jsmith@geemail.com', 'pass' => Hash::make('secret'), 'phone' => '5013217788', 'address1' => '1 Dirt Rd Way', 'address2' => '', 'city' => 'Romance', 'state' => 'AR', 'zipcode' => '72136', 'country' => 'USA'],
+                ['name' => 'Chris Gnome', 'email' => 'gnome@geemail.com', 'pass' => Hash::make('secret'), 'phone' => '2132224567', 'address1' => '313 Steel Rd', 'address2' => 'Unit 100', 'city' => 'Penciltucky', 'state' => 'PA', 'zipcode' => '47898', 'country' => 'USA'],
+                ['name' => 'Henry Tudor', 'email' => 'henryt@geemail.com', 'pass' => Hash::make('secret'), 'phone' => '9545555001', 'address1' => '615 Washington St.', 'address2' => 'Apt 2', 'city' => 'Rollywood', 'state' => 'FL', 'zipcode' => '33333', 'country' => 'USA']
+                
         );
             
         // Loop through each user above and create the record for them in the database
